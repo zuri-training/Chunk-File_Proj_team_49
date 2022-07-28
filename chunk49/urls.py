8f2dfc0487b114fd09fed8adb261a8da9bcc4002
Eruanga <acleo425@gmail.com>
2022-07-28 12:26:52 +0100

    minor chanages

--- chunk49/urls.py
+++ chunk49/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
 =======
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
+<<<<<<< HEAD
+>>>>>>> 3e01832424c2b4aec878019e241735303ecd4a2d
+=======
 >>>>>>> 3e01832424c2b4aec878019e241735303ecd4a2d
     path( '', include('chunkapp.urls') ),
 ]
