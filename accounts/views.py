2fd25b057a449fc49f9746c53377a982fd543336
Sophia Edeki <sophiesse143@gmail.com>
2022-07-28 06:08:04 +0100

    created a sign up page and a password change status page
    sign up page takes in user details, validates inputs and creates account if requirements are met, or allows previous    users to sign in.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request, 'register.html')
+def password_change_done(request):
+    return render(request, 'password_change_done.html')
---
+++ static/accounts/js/script.js
@@ -0,0 +1,107 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	checkInputs();
+	if(validForm() == true) {
+		form.submit ();
+	} else {
+		e.preventDefault();
+	}
+});
+
+function validForm() {
+	const inputContainers = form.querySelectorAll('.input-control');
+	let result = true;
+
+	inputContainers.forEach((container) => {
+		if(container.classList.contains('error')){
+			result  = false;
+		}
+	});
+	return result;
+}
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	}else if(usernameValue.length > 20){
+		setErrorFor(username, 'Username must be less than 15');
+	}
+	 else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Confirm password cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'input-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'input-control success';
+}
+	
+function isEmail(email) {
+
+	const checkemail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+	const emailResult = checkemail.test(email);
+	return emailResult
+
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
