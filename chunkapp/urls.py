b529e4cdea877aacc21440a99935d3032a2af3f2
Eruanga <acleo425@gmail.com>
2022-07-28 09:34:47 +0100

    included static files

--- chunkapp/urls.py
+++ chunkapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('',views.index,name='landingpage'),
     path('dashboard/', views.dashboard, name="dashboard"),
    
 ]
--- chunkapp/views.py
+++ chunkapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, 'chunkapp/dashboard.html', { })
+
+def index(request):
+    return render(request, 'chunkapp/index.html')
