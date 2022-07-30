91f49018bd9422f748cef47ff5c65a53addf19c3
Elisha Adeboye <y.adeboye@gmail.com>
2022-07-30 21:51:32 +0100

    added template tags for register.html

--- chunk49/urls.py
+++ chunk49/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('django.contrib.auth.urls')),
+    # path('accounts/',include('django.contrib.auth.urls')),
     path( '', include('chunkapp.urls') ),
+    path( 'accounts/', include('accounts.urls') ),
 ]
