be7ad26434a9456a4b096eb66c63989d808bce46
okputu julius <juliusstan10@gmail.com>
2022-07-30 23:34:05 +0100

    add function that list recent chunks

--- chunkapp/urls.py
+++ chunkapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('dashboard/', views.renderDashBoardView, name="dashboard"),
     path('dashboard/chunkFile', views.chunkFileView, name="chunkFile"),
-    path('faq',views.faq,name='faq')
+    path('faq',views.faq,name='faq'),
+    path('recent/',views.listRecentChunks,name='recent')
    
 ]
--- chunkapp/views.py
+++ chunkapp/views.py
@@ -7,22 +7,20 @@ from . models import ChunkOrder
 # understanding of code 
 # the view functions use a camelCase convention
 
+#landing page view
 def index(request):
     return render(request,'chunkapp/index.html')
-
+#frequently asked questions view
 def faq(request):
     return render(request,'chunkapp/faq.html')
-
-
-
-
+#dashboard view
 def renderDashBoardView(request):
     # this views primary function is too render a template
     # and then pass a form as the context
     chunk_order_form = ChunkOrderForm()
     context = {"form": chunk_order_form}
     return render(request, 'chunkapp/dashboard.html', context)
-
+#chunk file view
 def chunkFileView(request):
     # this views primary function is to chunk the uploaded files
     # it is also responsible for validating the chunk_order_form
@@ -48,3 +46,10 @@ def chunkFileView(request):
             # this else case shows that chunk order request was invalid
             # it then redirects the user back to the dashboard
             redirect("dashboard")
+#list recent chunks view
+def listRecentChunks(request):
+    recent_chunks=ChunkOrder.objects.all()
+    context={
+        'recent_chunks':recent_chunks
+    }          
+    return render(request,'chunkapp/recent.html',context)
