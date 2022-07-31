2374fd2035a11bf34663d391f96df11859e3fadf
Elisha Adeboye <y.adeboye@gmail.com>
2022-07-31 20:41:47 +0100

    created the custom user model form and customized it with the html css and JS

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-
-
+from .forms import SignUpForm
 from django.contrib.auth import login, authenticate 
 from django.contrib.auth.forms import UserCreationForm
 
@@ -15,20 +14,14 @@ def password_change_done(request):
 
 def register(request):
     if request.method == 'POST':
-        firstname = request.POST['fullname'].split(' ')[0]
-        lastname = request.POST['fullname'].split(' ')[1]
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        email = request.POST['email']
-        if password1 == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request, 'Email taken')
-            else:
-                user = User.objects.create_user(firstname = firstname, lastname = lastname, password = password1, email = email)
-                user.save()
-                print('user created successfully')
-        else:
-            print('password not matching..')
-    else:
-        return render(request, 'accounts/register.html')
-    return redirect('/')
+        form = SignUpForm(request.POST) 
+        if form.is_valid():
+            form.save()  
+            print('valid form')
+    form = SignUpForm() 
+    print('invalid form')
+    context = { 
+            'form': form 
+        } 
+
+    return render(request, 'accounts/register.html', context)
