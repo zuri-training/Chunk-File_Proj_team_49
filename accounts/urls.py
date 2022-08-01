38e31ad5a054e9da3e85bfd7b39e9003a19f86d9
okputu julius <juliusstan10@gmail.com>
2022-08-01 20:47:50 +0100

    fixing url conflicts

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
-<<<<<<< HEAD
-
-=======
     path('login/', views.login, name="login"),
->>>>>>> 48c98efdaa9155b2b5a56b90271c7e4435c2e842
 ]
