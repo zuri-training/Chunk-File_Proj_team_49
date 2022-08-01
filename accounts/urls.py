b77707fb0731bc785f181adeb83eb91e85839d6a
okputu julius <juliusstan10@gmail.com>
2022-08-01 20:44:38 +0100

    extra fixes

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
-    path('login/', views.login, name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
 ]
