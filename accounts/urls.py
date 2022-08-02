4b293c55e28f723d848044c0fe743c76db566f06
okputu julius <juliusstan10@gmail.com>
2022-08-02 01:37:56 +0100

    feat:
    --add all auth views
    --subclass all auth forms
    --set login and logout redirecr url in settings
    --map all necesary accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,16 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 
+
 urlpatterns = [
     path('register/', views.register, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+    path('password_change/',views.PasswordChange.as_view(),name='password_change'),
+    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),name='password_change_done'),
+    path('password_reset/',views.PasswordReset.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',views.PasswordResetConfirm.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_complete.html'),)
+
 ]
