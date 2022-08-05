ef1f7c85e78e940a31aeee0c8eb621e58ff774fc
sydney <sydney.idundun@gmail.com>
2022-08-05 21:30:19 +0200

    preparing heroku deployment

--- chunk49/settings.py
+++ chunk49/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
